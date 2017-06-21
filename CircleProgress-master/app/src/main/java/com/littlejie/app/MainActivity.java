eaba84b2f8a8e044f83d4a3cd58564e1723f7edd
https://github.com/leepears/CircleProgress <1395772579@qq.com>
2017-06-21 10:13:58 +0800

    1.0圆形进度条

---
+++ CircleProgress-master/app/src/main/java/com/littlejie/app/MainActivity.java
@@ -0,0 +1,75 @@
+package com.littlejie.app;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.littlejie.circleprogress.CircleProgress;
+import com.littlejie.circleprogress.DialProgress;
+import com.littlejie.circleprogress.WaveProgress;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final static int[] COLORS = new int[]{Color.RED, Color.YELLOW, Color.GREEN, Color.BLUE};
+
+    private Button mBtnResetAll;
+    private CircleProgress mCircleProgress1, mCircleProgress2, mCircleProgress3;
+    private DialProgress mDialProgress;
+    private WaveProgress mWaveProgress;
+    private Random mRandom;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBtnResetAll = (Button) findViewById(R.id.btn_reset_all);
+        mCircleProgress1 = (CircleProgress) findViewById(R.id.circle_progress_bar1);
+        mCircleProgress2 = (CircleProgress) findViewById(R.id.circle_progress_bar2);
+        mCircleProgress3 = (CircleProgress) findViewById(R.id.circle_progress_bar3);
+        mDialProgress = (DialProgress) findViewById(R.id.dial_progress_bar);
+        mWaveProgress = (WaveProgress) findViewById(R.id.wave_progress_bar);
+
+        mBtnResetAll.setOnClickListener(this);
+        mCircleProgress1.setOnClickListener(this);
+        mCircleProgress2.setOnClickListener(this);
+        mCircleProgress3.setOnClickListener(this);
+        mDialProgress.setOnClickListener(this);
+        mWaveProgress.setOnClickListener(this);
+
+        mRandom = new Random();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_reset_all:
+                mCircleProgress1.reset();
+                mCircleProgress2.reset();
+                mCircleProgress3.reset();
+                mDialProgress.reset();
+                break;
+            case R.id.circle_progress_bar1:
+                mCircleProgress1.setValue(mRandom.nextInt((int) mCircleProgress1.getMaxValue()));
+                break;
+            case R.id.circle_progress_bar2:
+                mCircleProgress2.setValue(mRandom.nextFloat() * mCircleProgress2.getMaxValue());
+                break;
+            case R.id.circle_progress_bar3:
+                //在代码中动态改变渐变色，可能会导致颜色跳跃
+                mCircleProgress3.setGradientColors(COLORS);
+                mCircleProgress3.setValue(mRandom.nextFloat() * mCircleProgress3.getMaxValue());
+                break;
+            case R.id.dial_progress_bar:
+                mDialProgress.setValue(mRandom.nextFloat() * mDialProgress.getMaxValue());
+                break;
+            case R.id.wave_progress_bar:
+                mWaveProgress.setValue(mRandom.nextFloat() * mWaveProgress.getMaxValue());
+                break;
+        }
+    }
+}
---
+++ CircleProgress-master/readme.md
@@ -0,0 +1,16 @@
+# 圆形进度条
+Android 自定义圆形进度条，包括传统传统的圆形(CircleProgress)、刻度(DialProgress)、水波纹(WaveProgress)效果的三种圆形进度条。
+
+效果图如下：
+
+![圆形进度条](circle.gif)
+
+![刻度及水波纹进度条](dialandwave.gif)
+
+关于圆形进度条的总结请见：[Android 自定义圆形进度条总结](http://www.jianshu.com/p/5cec84ea08d3)
+
+## XML 配置
+关于 XML 的配置请见 circleprogress module 下的 [attrs.xml](circleprogress/src/main/res/values/attrs.xml) 文件
+
+## 联系方式
+邮箱:1025263614lsj@gmail.com
